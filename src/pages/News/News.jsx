9628ce1a0e598a69bbe7dabdca91eb243fdf60fe
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-09-08 05:01:22 +0800

    Membuat news page dan mengganti modalnya

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -13,16 +13,16 @@ function News() {
               <h1 id="tagline" class="section-heading">
                 Artikel
               </h1>
-              <h5 class="section-subheading">Info menarik untuk bayi anda</h5>
+              <h5 class="section-subheading">Info menarik untuk bayi anda dari sumber terpercaya</h5>
               <hr />
             </div>
           </div>
           <div class="container-fluid">
-            <div class="row">
+            <div class="row d-flex justify-content-center">
               {datas.map((data) => (
                 <div class="col-lg-4 col-sm-6 mb-4">
                   <div key={data.id}>
-                    <CardNews title={data.title} url={data.url} deskripsi={data.deskripsi} id={data.id} />
+                    <CardNews title={data.title} picture={data.picture} desc={data.desc} URL={data.URL} id={data.id} />
                   </div>
                 </div>
               ))}
