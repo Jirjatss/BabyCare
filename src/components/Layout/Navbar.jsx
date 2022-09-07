b8df75a0a54a5b3e017d08602d89b50a7587d4d5
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-09-07 19:32:29 +0800

    menambahkan fitur about us di HomePage

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -7,15 +7,13 @@ const Navbar = () => {
       <section id="jumbotron" class="jumbotron jumbotron-fluid">
         <nav class="navbar navbar-expand-lg fixed-top">
           <div class="container">
-            <a class="navbar-brand" to="/">
+            <a class="navbar-brand" href="/">
               <img style={{ width: 50 }} src={require("../../assets/brand.png")} alt="" />
             </a>
             <a class="navbar-brand" href="">
               Login
             </a>
-            <a class="navbar-brand" href="">
-              Consultation
-            </a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon" style={{ color: "whitesmoke" }}>
                 Menu
@@ -23,16 +21,28 @@ const Navbar = () => {
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="">
+                <div class="dropdown" style={{ marginTop: "0.1rem" }}>
+                  <a style={{ color: "white" }} class="btn dropdown-toggle" href="" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
                     About
                   </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="">
-                    Services
-                  </a>
-                </li>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <li>
+                      <a class="dropdown-item" href="#About">
+                        About Us
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="">
+                        Services
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="">
+                        News
+                      </a>
+                    </li>
+                  </ul>
+                </div>
                 <li class="nav-item">
                   <a class="nav-link" href="#ourteam">
                     Team
@@ -41,6 +51,11 @@ const Navbar = () => {
                 <li class="nav-item">
                   <a class="nav-link" href="">
                     Baby Shop
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="">
+                    Consultation
                   </a>
                 </li>
               </ul>
