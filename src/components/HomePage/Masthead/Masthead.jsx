4d918af04b66abc9056d62aeb7f7bcfb042ad7b1
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-09-09 00:33:47 +0800

    memperbaiki css masthead

--- src/components/HomePage/Masthead/Masthead.jsx
+++ src/components/HomePage/Masthead/Masthead.jsx
@@ -5,12 +5,20 @@ function Masthead() {
   return (
     <>
       <div className="container-fluid halaman">
-        <div class="container" style={{ borderRadius: "10px", padding: "1rem" }}>
-          <div class="flex justify-content items-center">
+        <div className="container" style={{ borderRadius: "10px", padding: "1rem" }}>
+          <div className="flex justify-content items-center">
             <div>
-              <img src={"https://see.fontimg.com/api/renderfont4/K7axe/eyJyIjoiZnMiLCJoIjoxMTYsInciOjEyNTAsImZzIjo5MywiZmdjIjoiIzEyMTIxMiIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/QmFieSBDYXJl/hugh-is-life-personal-use-italic.png"} alt="" />
+              <img
+                className="poster"
+                src={"https://see.fontimg.com/api/renderfont4/K7axe/eyJyIjoiZnMiLCJoIjoxMTYsInciOjEyNTAsImZzIjo5MywiZmdjIjoiIzEyMTIxMiIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/QmFieSBDYXJl/hugh-is-life-personal-use-italic.png"}
+                alt=""
+              />
               <br />
-              <img src={"https://see.fontimg.com/api/renderfont4/gv11/eyJyIjoiZnMiLCJoIjo1MSwidyI6MTI1MCwiZnMiOjQxLCJmZ2MiOiIjMTIxMjEyIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/eW91ciBiYWJ5IHNvbHV0aW9ucw/cookiemonster.png"} alt="" />
+              <img
+                className="poster"
+                src={"https://see.fontimg.com/api/renderfont4/gv11/eyJyIjoiZnMiLCJoIjo1MSwidyI6MTI1MCwiZnMiOjQxLCJmZ2MiOiIjMTIxMjEyIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/eW91ciBiYWJ5IHNvbHV0aW9ucw/cookiemonster.png"}
+                alt=""
+              />
             </div>
           </div>
         </div>
