1c0ad3470e71eaa24228dbd811049ab2297cfb24
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-09-08 19:21:39 +0800

    membuat konsultasi page

---
+++ src/pages/Consultation/Consultation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Consultation.css";
+
+function Consultation() {
+  return (
+    <>
+      <section class="page-section" id="consultation">
+        <div class="container">
+          <div class="text-center">
+            <h2 id="tagline" class="section-heading">
+              Consultation
+            </h2>
+            <h5 class="section-subheading">Konsultasikan kesehatan si kecil kepada kami</h5>
+            <hr />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Consultation;
