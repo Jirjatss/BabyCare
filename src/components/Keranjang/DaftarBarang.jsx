f6a9538bfa0d16237d61c3022ad9874ea8043ca2
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-09-19 08:06:38 +0800

    css

--- src/components/Keranjang/DaftarBarang.jsx
+++ src/components/Keranjang/DaftarBarang.jsx
@@ -10,48 +10,50 @@ function DaftarBarang(props) {
   Aos.init();
   return (
     <>
-      <div id="container" key={props.id}>
-        <div className="row d-flex-justify-content-center">
-          <div className="col-4">
-            <div className="product-details text-center">
-              <h4>{props.title}</h4>
+      <div className="col">
+        <div id="container" key={props.id}>
+          <div className="row d-flex-justify-content-center">
+            <div className="col-4">
+              <div className="product-details text-center">
+                <h4>{props.title}</h4>
 
-              <p className="information"></p>
+                <p className="information"></p>
+              </div>
             </div>
-          </div>
-          <div className="col-3 product-details">
-            <button className="minus ms-2 me-2">+</button>
-            <span>1</span>
-            <button className="minus ms-2 me-2">-</button>
-            <div>
-              <br />
-              <button className="plus ms-2" onClick={props.DelCart}>
-                <span style={{ textDecoration: "none", color: "black", fontWeight: "bolder" }}>
-                  <img className="mt-12" style={{ width: 30, height: 30 }} src={require("../../assets/au.png")} alt="" />
-                </span>
-              </button>
+            <div className="col-3 product-details">
+              <button className="minus ms-2 me-2">+</button>
+              <span>1</span>
+              <button className="minus ms-2 me-2">-</button>
+              <div>
+                <br />
+                <button className="plus ms-2" onClick={props.DelCart}>
+                  <span style={{ textDecoration: "none", color: "black", fontWeight: "bolder" }}>
+                    <img className="mt-12" style={{ width: 30, height: 30 }} src={require("../../assets/au.png")} alt="" />
+                  </span>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="col-5">
-            <div className="product-image">
-              <img src={props.url} alt="" />
+            <div className="col-5">
+              <div className="product-image">
+                <img src={props.url} alt="" />
 
-              <div className="info">
-                <h4> Description</h4>
-                <ul>
-                  <li>
-                    <strong>Kategori : </strong>
-                    {props.deskripsi}
-                  </li>
-                  <li>
-                    <strong>Harga : </strong>
-                    {harga}
-                  </li>
-                  <li>
-                    <strong>Kondisi : </strong>
-                    {props.Kondisi}
-                  </li>
-                </ul>
+                <div className="info">
+                  <h4> Description</h4>
+                  <ul>
+                    <li>
+                      <strong>Kategori : </strong>
+                      {props.deskripsi}
+                    </li>
+                    <li>
+                      <strong>Harga : </strong>
+                      {harga}
+                    </li>
+                    <li>
+                      <strong>Kondisi : </strong>
+                      {props.Kondisi}
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
